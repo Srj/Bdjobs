a82650665faf53ee12ad43d2e27ca00331d70b02
Saifur Rahman <srj.buet17@gmail.com>
2022-06-16 14:51:40 +0600

    Published

--- fetch.py
+++
@@ -1,6 +0,0 @@
-import wandb 
-import sys
-
-run = wandb.init('bdjobs')
-artifact = run.use_artifact(sys.argv[1])
-artifact_dir = artifact.download()
--- update.py
+++
@@ -1,9 +0,0 @@
-import wandb
-import sys
-run = wandb.init(project="bdjobs",job_type='Graph_Update')
-artifact = wandb.Artifact(sys.argv[1],type='dataset')
-if sys.argv[1] == 'all':
-    artifact.add_dir('./')
-else:
-    artifact.add_dir(sys.argv[1])
-run.log_artifact(artifact)
